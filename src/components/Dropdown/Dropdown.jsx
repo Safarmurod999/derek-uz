d980b2de33b2aed8cc6ebc2fd156168b16691a7f
Safarmurod999 <supermen0904@gmail.com>
2024-07-07 20:15:58 +0500

    fixes

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -1,13 +1,20 @@
-const Dropdown = ({ path, title, style, links }) => {
+import { useNavigate } from "react-router-dom"
+
+const Dropdown = ({ path, title, style, links, setOpenNavbar }) => {
+  const navigate = useNavigate();
   return (
     <div className={`dropdown ${style}`}>
-      <a href={path} className={`dropdown__button `}>
+      <a href={path} onClick={(e) => {
+        // e.preventDefault();
+        navigate(path);
+        setOpenNavbar(false);
+      }} className={`dropdown__button `}>
         {title}
       </a>
       <div className="dropdown__content">
         {
           links.map((link) => {
-            return <a key={link.id} href={link.path}>{link.title}</a>
+            return <a key={link.id} href={link.path} >{link.title}</a>
           })
         }
       </div>
